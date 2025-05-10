852edf7912bd7fc46a02f5a42fa74ffd9882453d
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2025-05-10 11:56:15 -0700

    update scene

--- bin/single_render.ml
+++ bin/single_render.ml
@@ -1,7 +1,7 @@
 open Ray_tracer
 open Phong_tracer
 
-let scene = Scenes.space_scene 300 0.9
+let scene = Scenes.space_scene 800 0.9
 (* let scene = Scenes.room_scene 300 0.9 *)
 (* let scene = Scenes.spheres_scene 300 0.9 *)
 (* let scene = Scenes.obj_scene 800 0.1 *)
